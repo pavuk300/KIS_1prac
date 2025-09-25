765dbb969d7b0d8aa109b8364ec546170483a7dd
Anikin Evgeniy <ani.evgeniy@yandex.ru>
2025-09-25 14:11:43 +0300

    2 stage

--- REPL.py
+++ REPL.py
@@ -1,15 +1,31 @@
-import os
+from sys import argv
+from os import path
+
+
+def startup():
+	if len(argv) != 3:
+		print("invalid set of arguments")
+		exit()
+	if "C:\\" not in argv[1]:
+		argv[1] = path.dirname(__file__) + "\\" + argv[1]
+	if "C:\\" not in argv[2]:
+		argv[2] = path.dirname(__file__) + "\\" + argv[2]
+	if not (path.exists(argv[1]) or path.exists(argv[2])):
+		print("invalid set of arguments")
+		exit()
+	print("\nVfs location -> " + argv[1])
+	print("Startup script -> " + argv[2], end="\n\n")
 
 
 def cd(argc):  # функция, вызываемая по команде cd
-	print("cd " + " ".join([i.strip('"') for i in argc]))
+	return "cd " + " ".join([i.strip('"') for i in argc])
 
 
 def ls(argc):  # функция, вызываемая по команде ls
-	print("ls " + " ".join([i.strip('"') for i in argc]))
+	return "ls " + " ".join([i.strip('"') for i in argc])
 
 
-class unix_emulator:
+class UnixEmulator:
 	dir = "VFS"
 
 	commands = {  # словарь команд и соответсвующих функций функций
@@ -18,23 +34,36 @@ class unix_emulator:
 		"exit": exit
 	}
 
-	def mainLoop(self):
+	def init_command(self, com, argc):
+		if com in self.commands:  # если команда имеется в словаре commands, запускаем соответствующую функцию
+
+			# если в списке argc имеются аргументы данные без кавычек -> выводим ошибку
+			if [i for i in argc if i[0] != '"' or i[-1] != '"']:
+				return [com + ": invalid arguments", False]
+			return [self.commands[com](argc), True]
+		else:
+			return [com + ": no such command", False]  # иначе выводим ошибку
+
+	def startup_script(self):
+		out = ""
+		print("Startup script:\n")
+		with open(argv[2]) as f:
+			for i in f:
+				com, *argc = i.split()
+				buf = self.init_command(com, argc)
+				if not buf[1]:
+					print("The script terminated with an error\n")
+					return
+				out += buf[0] + "\n"
+		print(out)
+
+	def main_loop(self):
+		startup()
+		self.startup_script()
 		while True:
-			if not os.path.exists("VFS"):  # Если не существует корневой папки VFS -> создаем ее
-				os.mkdir("VFS")
-				self.dir = "VFS"
 			print(self.dir + " >", end=" ")  # вывод приглашения к вводу
 			com, *argc = input().split()  # ввод команды и аргрументов
-			if com in self.commands:  # если команда имеется в словаре commands, запускаем соответствующую функцию
-
-				# если в списке argc имеются аргументы данные без кавычек -> выводим ошибку
-				if [i for i in argc if i[0] != '"' or i[-1] != '"']:
-					print(com + ": invalid arguments")
-					continue
-				self.commands[com](argc)
-			else:
-				print(com + ": no such command")  # иначе выводим ошибку
+			print(self.init_command(com, argc)[0])
 
 
-init = unix_emulator()
-init.mainLoop()
+UnixEmulator().main_loop()
